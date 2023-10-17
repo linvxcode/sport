d4ab2835c3f3b44e78eb6a20a74b18d791b03e03
linxcode <linxmusic.com>
2023-10-17 21:41:34 +0700

    hmmm

--- src/common/modules/Mobile/index.tsx
+++ src/common/modules/Mobile/index.tsx
@@ -1,13 +1,20 @@
+'use client'
 import React from 'react'
+import {motion, AnimatePresence} from 'framer-motion'
 
 export const Mobile = () => {
   return (
-    <div>
+    <AnimatePresence>
+    <motion.div 
+    initial={{translateY: 50, opacity: 0}}
+    whileInView={{ translateY:0, opacity: 1, transition: {type: "spring", duration: 2}}}
+    >
       <div className='flex justify-center items-center'>
         <h1 className='text-7xl text-center'>
             Coming Soon..!
         </h1>
       </div>
-    </div>
+    </motion.div>
+    </AnimatePresence>
   )
 }
